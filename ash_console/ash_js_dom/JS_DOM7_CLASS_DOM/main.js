d040783ab692da82fb04d26a71fd9f1bc0acb739
ashgraey <123621863+ashgraey@users.noreply.github.com>
2023-02-10 14:41:44 +0900

    0210

--- ash_console/ash_js_dom/JS_DOM7_CLASS_DOM/main.js
+++ ash_console/ash_js_dom/JS_DOM7_CLASS_DOM/main.js
@@ -1,9 +1,9 @@
-//main.js에서 play.js 불러오기
-//현재 예제에서는 main.js와 play.js 파일은 
+//main.js에서 game.js 불러오기
+//현재 예제에서는 main.js와 game.js 파일은
 //같은 경로에 있다는 것을 가정
 
-import {Game} from './game.js';
-var $titleButton = document.querySelector('#title-button');
-$titleButton.addEventListener('click', (event) => {
-    new Game();
+import { Game } from "./game.js"; // game.js 안에 있는 class game
+var $titleButton = document.querySelector("#title-button");
+$titleButton.addEventListener("click", (event) => {
+  new Game(); // game 클래스 호출
 });
