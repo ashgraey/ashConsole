1962cd96fe915dae221593f4c58fe648f4bdece0
ashgraey <123621863+ashgraey@users.noreply.github.com>
2023-02-14 15:24:45 +0900

    0214

---
+++ ash_console/ash_js_ui/_main.js
@@ -0,0 +1,24 @@
+import { ManagerGame } from "./managerGame.js"; // js에서 다른 class import
+
+function draw() {
+  ctx.clearRect(0, 0, $canvas.width, $canvas.height);
+  //-----------------------------------------------
+  ManagerGame.getInstance().update();
+  ManagerGame.getInstance().draw();
+  //-----------------------------------------------
+}
+function loading() {
+  //추후 로딩 구현
+
+  setInterval(draw, 10);
+}
+
+//-----------[캔버스]----------------------------
+var $canvas = document.getElementById("myCanvas");
+var ctx = $canvas.getContext("2d");
+//-----------[시작]----------------------------
+ManagerGame.getInstance().start(ctx);
+
+$canvas.style.border = "1px solid black"; // 외곽선
+
+setTimeout(loading, 10);
