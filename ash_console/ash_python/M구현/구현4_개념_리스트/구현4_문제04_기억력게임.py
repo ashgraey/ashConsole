e65c04a0eb8ea040719eb98885db657412f53b47
ashgraey <123621863+ashgraey@users.noreply.github.com>
2023-02-20 11:49:14 +0900

    .

--- ash_console/ash_python/M구현/구현4_개념_리스트/구현4_문제04_기억력게임.py
+++ ash_console/ash_python/M구현/구현4_개념_리스트/구현4_문제04_기억력게임.py
@@ -10,5 +10,50 @@
 			[2] 이미 선택해서 맞춘카드는 다시 선택할수없다.
 		
 """
+import random
 
+class memoryGame :
+    def __init__(self, c) :
+        self.card = c
+        self.count = 0
+        self.run()
+        
+    def cardShuffle(self) :
+        for _ in range(100) :
+            r = random.randint(0, len(self.card) - 1)
+            temp = self.card[0]
+            self.card[0] = self.card[r]
+            self.card[r] = temp
+    
+    
+    def breakCk(self) :
+        if self.count == 5 :
+            print("게임종료")
+            return False
+        return True
+        
+	
+    def	run(self) :
+        self.cardShuffle()
+        
+        while self.breakCk() :
+            print(self.card)
+            print("입력1 : 입력2 : ")
+            idx1, idx2 = map(int, input().split())
+            
+            if idx1 == idx2 : 
+                continue
+            
+            if self.card[idx1] == self.card[idx2] :
+                   print("정답")
+                   self.count += 1 
+            else :
+                print("오답")
+            
+            # if self.count == 5 : 
+            #     break
+	
 card = ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd', 'e', 'e' ]
+memoryGame = memoryGame(card)
+# memoryGame.cardShuffle()
+# print(memoryGame.card)
