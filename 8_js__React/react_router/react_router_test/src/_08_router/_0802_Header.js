65abeff9e14d05df49a578871553f53c5ace49e5
ashgraey <123621863+ashgraey@users.noreply.github.com>
2023-04-13 14:21:25 +0900

    s

---
+++ 8_js__React/react_router/react_router_test/src/_08_router/_0802_Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function _0802_Header() {
+    return (
+        <>
+            <div className="container bg-light">
+                <div className="row mb-3">
+                    <div className="col ">
+                        <div className="d-flex justify-content-center">
+                            <img src="logo1000.png" width="150"></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default _0802_Header
---
+++ 8_js__React/react_router/react_router_test/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import _0801_Controller from './_08_router/_0801_Controller';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.Fragment>
+    <_0801_Controller>hello world</_0801_Controller>
+    
+  </React.Fragment>
+
+);
+
